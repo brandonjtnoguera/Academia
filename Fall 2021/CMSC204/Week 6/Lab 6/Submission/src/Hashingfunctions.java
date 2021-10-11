674fa8d7956aac0e1af54f00afc80b807ce96437
Brandon <bran.ten25@gmail.com>
2021-10-10 23:35:00 -0400

    lol

---
+++ Fall 2021/CMSC204/Week 6/Lab 6/Submission/src/Hashingfunctions.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Hashingfunctions {
+    public static void main(String[] args) {
+        // 15  54  13  10  135  114  49  174  27  24
+        int n = 1;
+        System.out.println((24 + (n*n)) % 13);
+
+    }
+}
