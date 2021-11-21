bcb839b79a11e82bc3388a441c6d9de9c2b49936
bran-ten <bran.ten25@gmail.com>
2021-11-21 16:02:57 -0500

    finished lab 11

--- Fall 2021/CMSC204/Lab 11 (Multithreading)/Submission/src/CarQueue.java
+++ Fall 2021/CMSC204/Lab 11 (Multithreading)/Submission/src/CarQueue.java
@@ -1,2 +1,34 @@
+import java.util.*;
+
+/**
+ * @author Brandon Tenorio
+ */
 public class CarQueue {
+    Queue<Integer> queue = new ArrayDeque<>();
+    Random random = new Random();
+
+    public CarQueue(){
+        for(int i = 0; i < 5; i++) queue.add(random.nextInt(4));
+    }
+
+    public void addToQueue(){
+        new Thread(() -> {
+            try{
+                queue.add(random.nextInt(4));
+                queue.add(random.nextInt(4));
+                queue.add(random.nextInt(4));
+                queue.add(random.nextInt(4));
+                queue.add(random.nextInt(4));
+                Thread.sleep(1000);
+
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public int deleteQueue(){
+        if(queue.isEmpty()) return 0;
+        else return queue.remove();
+    }
 }
